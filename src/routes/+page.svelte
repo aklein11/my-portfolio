c1af8efdf733ba8517ea7bcb037bdb4f18d4a366
Abigail <aklein11@mit.edu>
2024-03-06 15:23:57 -0500

    trying to delete redundant nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,14 +9,6 @@
 
 <h1>Abigail Klein</h1>
 
-<!-- <nav>
-    <a hreft="index.html" padding-bottom>Home</a>
-    <a href="contact/index.html">Contact</a>
-    <a href="projects/index.html">Projects</a>
-    <a href="https://github.com/aklein11/" target="_blank">GitHub Profile</a>
-    <a href="resume/index.html">Resume</a>
-</nav> -->
-
 <p>My name is Abigail Klein and I am a senior studying Computation and Cognition at MIT. I enjoy coding and am excited to learn more web development and data visualization techniques through this class.</p>
 
 <img src="images/googleBlurbPicture.jpg" alt="portrait of Abigail" width="450"/>
